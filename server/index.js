25f57ed939ea3cd8c653290bfc4c5a8228c71aaf
Tanmay11 <tanmaybhansali07@gmail.com>
2024-06-03 18:56:09 +0530

    Real time conversation not working so in order to solve it this is one of the attempt

--- server/index.js
+++ server/index.js
@@ -4,7 +4,6 @@ const express=require("express")
 const http=require("http")
 const {Server}=require("socket.io")
 const cookieParser=require("cookie-parser")
-const {socketHandler}=require("./socketHandler.js")
 const AuthRouter=require("./routes/auth.js")["router"]
 const UserRouter=require("./routes/user.js")["router"]
 const PostRouter=require("./routes/post.js")["router"]
@@ -27,13 +26,50 @@ server.use("/api/users",UserRouter)
 server.use("/api/posts",PostRouter)
 server.use("/api/chats",ChatRouter)
 server.use("/api/messages",MessageRouter)
-socketHandler(io)
 
 main().catch(err => console.log(err));
 
 async function main() {
   await mongoose.connect(`mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.rt3uxtg.mongodb.net/Real-Estate-db?retryWrites=true&w=majority&appName=Cluster0`,{ignoreUndefined:true});
 }
+
+let onlineUsers=[]
+
+io.on("connection",(socket)=>{
+
+  socket.on("user-connected",(data)=>{
+      onlineUsers.push(data)
+  })
+
+  socket.on("chat-connected",async (data)=>{
+      await Message.updateMany({chatId:data.chatId,seenBy:{$nin:data.userId}},{$push:{seenBy:data.userId}})
+      const index=onlineUsers.findIndex(user=>user.userId===data?.userId)
+      onlineUsers[index]={...onlineUsers[index],chatId:data.chatId}
+  })
+
+  socket.on("close-chat",(data)=>{
+    onlineUsers=onlineUsers.map(user=>user.userId===data.userId ? {userId:user?.userId,socketId:user?.socketId} : user)
+  })
+
+  socket.on("send-message",async (data)=>{
+    const receiver=onlineUsers.find(user=>user.userId===data.receiverId)
+    if(!receiver)return
+    if(receiver.chatId===data.chatId){
+      await Message.findOneAndUpdate({_id:data.messageObj?.messageId},{$push:{seenBy:receiver?.userId}})
+      return socket.to(receiver.socketId).emit("receive-message",{message:data.messageObj})
+    }
+    socket.to(receiver.socketId).emit("receive-notification",{chatId:data.chatId,message:data.messageObj})
+  })
+
+  socket.on("user-disconnect",(userId)=>{
+    onlineUsers=onlineUsers.filter(user=>user.userId!==userId)
+  })
+
+  socket.on("disconnect",()=>{
+    console.log("disconnected")
+  })
+
+})
 
 httpServer.listen(process.env.PORT)
 
