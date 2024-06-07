8cab86b61a1629fd5a0e03fc0a9fd64dcb9b9592
Tanmay11 <tanmaybhansali07@gmail.com>
2024-06-07 10:10:57 +0530

    Fixed the bug present on socketAuth File

--- server/middleware/socketAuth.js
+++ server/middleware/socketAuth.js
@@ -2,8 +2,8 @@ const cookie=require("cookie")
 const jwt=require("jsonwebtoken")
 
 function socketAuth(socket,next){
-const jsonCookie=cookie.parse(socket.handshake.headers.cookie)
-if(!jsonCookie.token)next(new Error("Token not found please login"))
+const jsonCookie=cookie.parse(socket.handshake.headers.cookie || "")
+if(!jsonCookie?.token)next(new Error("Token not found please login"))
 
   jwt.verify(jsonCookie.token,process.env.JWT_SECRET_KEY,(err,payload)=>{
     if(err)next(new Error("Invalid token"))
